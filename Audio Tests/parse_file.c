78b4a022dbbd598eca4b17680c173abbe4cb11f1
Thomas Sarrazin <tomsarrazin01@gmail.com>
2026-03-25 21:00:24 -0400

    more error checks

--- Audio Tests/parse_file.c
+++ Audio Tests/parse_file.c
@@ -2,9 +2,19 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    FILE *fp = fopen("./gloop_v2.wav", "rb"); 
+    char *filename = argv[1];
+
+    FILE *fp = fopen(argv[1], "rb"); 
     if (fp == NULL) {
-        perror("Error reading file. Ensure the file is the first argument after the program name.");
+        perror("Error reading file");
+        exit(1);
+    }
+
+    int name_length = strlen(argv[1]);
+    char file_ex[5] = {filename[name_length - 4], filename[name_length - 3], filename[name_length - 2], filename[name_length - 1], '\0'};
+
+    if (strcmp(file_ex, ".wav") != 0) {
+        printf("File is not .wav");
         exit(1);
     }
 
@@ -13,31 +23,31 @@ int main(int argc, char *argv[]) {
     
     // Reading number of channels
     if (fseek(fp, 22, SEEK_SET) == -1) {
-        perror("File smaller than .wav header (num_channels).");
+        perror("File smaller than .wav header (num_channels)");
         exit(1);
     }
     if (fread(&num_channels, 2, 1, fp) == 0 || num_channels > 2) {
-        perror("Error reading number of channels. Ensure the file does not have more than 2 audio channels");
+        printf("Error reading number of channels. Ensure the file does not have more than 2 audio channels");
         exit(1);
     }
 
     // Reading sample rate
     if (fseek(fp, 24, SEEK_SET) == -1) {
-        perror("File smaller than .wav header (sample_rate).");
+        perror("File smaller than .wav header (sample_rate)");
         exit(1);
     }
     if (fread(&sample_rate, 4, 1, fp) == 0) {
-        perror("Error reading sample rate.");
+        printf("Error reading sample rate.");
         exit(1);
     }
 
     // Reading bit depth
     if (fseek(fp, 34, SEEK_SET) == -1) {
-        perror("File smaller than .wav header (bit_depth).");
+        perror("File smaller than .wav header (bit_depth)");
         exit(1);
     }
     if (fread(&bit_depth, 2, 1, fp) == 0) {
-        perror("Error reading bit depth.");
+        printf("Error reading bit depth.");
         exit(1);
     }
 
