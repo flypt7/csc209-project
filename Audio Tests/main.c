e4884018f69ac92fd544b250207d1979bf836b42
Nathan <Nathan.bergman@mail.utoronto.ca>
2026-03-26 00:21:23 -0400

    more prepare_data testing

--- Audio Tests/main.c
+++ Audio Tests/main.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <fftw3.h>
+#include <stdlib.h>
+#include "parse_file.h"
 #include "prepare_data.h"
 
 int main() { 
-double data[5] = {100,2000,300,4000,500};
-int bit_depth = 12;
-int size = 5;
-double* newdata = prepare_data(data, bit_depth, size);
-for(int i = 0; i < size; i++){
-        printf("%f\n",newdata[i]);
-    }
+WAV_INFO * info = parse_file("Owls.wav");
+double* newdata = prepare_data(info->right_channel_pcm)
+for(int i = 0; i < 10; i++){
+    printf("%f/n",info->right_channel_pcm[i])
+}
+for(int i = 0; i < 10; i++){
+    printf("%f/n",newdata[i])
+}
+
+fftw_complex *in, *out;
+fftw_plan p;
+in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+for(int i = 0; i < info->pcm_size; i++){
+    in[i]=newdata[i];
+}
+
+fftw_execute(p); /* repeat as needed */
+
+for(int i = 0; i < 10; i++){
+    printf("%f+%fi\n",out[i][0],out[i][1]);
+}
+
+fftw_destroy_plan(p);
+fftw_free(in); fftw_free(out);
 }
