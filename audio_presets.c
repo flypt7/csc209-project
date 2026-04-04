06376c8549a3a5c9605515f80b68239d6e3c4e5c
Bergman Nathan Andrew <bergma43@teach.cs.toronto.edu>
2026-04-04 17:53:52 -0400

    audio presets

---
+++ audio_presets.c
@@ -0,0 +1,14 @@
+int* select_bands(char* selection){
+    if(strcmp(selection,"bb")){
+        return {1,1,1,1,1,1,1,1};
+    }
+    if(strcmp(selection,"bb")){
+        return {1,1,1,1,1,1,1,1};
+    }
+    if(strcmp(selection,"bb")){
+        return {1,1,1,1,1,1,1,1};
+    }
+    if(strcmp(selection,"bb")){
+        return {1,1,1,1,1,1,1,1};
+    }
+}
